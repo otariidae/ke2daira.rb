f66b619170c73f9c2bc7acbc515a0dca307e9070
otariidae <otariidae@users.noreply.github.com>
2025-07-02 02:05:13 +0900

    test: add some cases

--- spec/ke2daira_spec.rb
+++ spec/ke2daira_spec.rb
@@ -13,6 +13,18 @@ RSpec.describe Ke2daira do
     expect(described_class.ke2dairanize("ポール マッカートニー")).to eq("マール ポッカートニー")
   end
 
+  it "チェ ゲバラ", pending: "モーラ単位でのスワップは未対応" do
+    expect(described_class.ke2dairanize("チェ ゲバラ")).to eq("ゲ チェバラ")
+  end
+
+  it "加藤 あい" do
+    expect(described_class.ke2dairanize("加藤 あい")).to eq("アトウ カイ")
+  end
+
+  it "阿藤 快", pending: "快がココロヨと読まれてしまう" do
+    expect(described_class.ke2dairanize("阿藤 快")).to eq("カトウ アイ")
+  end
+
   it "森 進一" do
     expect(described_class.ke2dairanize("森 進一")).to eq("シリ モンイチ")
   end
